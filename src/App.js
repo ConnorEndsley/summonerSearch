6cfc0741b33098965eb9d0916f6d8d3422c9e88e
ConnorEndsley <endsleyconnor95@gmail.com>
2024-04-20 17:00:28 -0500

    links working:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css'
 import Home from './components/Home';
 import Nav from './components/Nav';
+import Champions from './components/Champions';
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Nav></Nav>
-      <Home></Home>
-    </div>
+
+<div className='App'>
+<BrowserRouter>
+<nav className="navContainer">
+        <h2 className="title"><Link to="/">Summoner Search</Link></h2>
+        <ul className="navList">
+          <li className="navItem"><Link to="/">Home</Link></li>
+          <li className="navItem"> <Link to="/champions">Champions</Link></li>
+          <li className="navItem" > <Link to="/matchHistory">Match History</Link></li>
+          <li className="navItem">Contact Us</li>
+        </ul>
+</nav>
+
+
+      <Routes>
+      <Route exact path="/" element={<Home></Home>}/>
+      <Route path='/champions' element={<Champions></Champions>}/>
+      </Routes>
+      </BrowserRouter>
+      </div>
+
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import "../styles/Nav.css";
 import { SvgIcon } from "@mui/material";
+import { Link } from "react-router-dom";
+
 
 const Nav = () => {
   return (
     <>
-      <nav className="navContainer">
-        <h2 className="title">Summoner Search</h2>
-        <ul className="navList">
-          <li className="navItem">Home</li>
-          <li className="navItem">Champions</li>
-          <li className="navItem">Match History</li>
-          <li className="navItem">Contact Us</li>
-          {/* <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li> */}
-        </ul>
-      </nav>
+
     </>
   );
 };
