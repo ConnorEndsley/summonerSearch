3fe1e82e71cf07884919f46b7d520e4e5fbbc587
Connor <endsleyconnor95@gmail.com>
2024-05-06 13:27:56 -0500

    working on it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import './App.css'
-import Home from './components/Home';
-import Nav from './components/Nav';
-import Champions from './components/Champions';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Champions from "./components/Champions";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-<Routes>
-  <Route path='/' element={<Home/>}/>
-  <Route path='/champions' element={<Champions/>}/>
-</Routes>
+    <>
+      <nav className="navContainer">
+        <h2 className="title">Summoner Search</h2>
+        <ul className="navList">
+          <li className="navItem">Home</li>
+          <li className="navItem">Champions</li>
+          <li className="navItem">Match History</li>
+          <li className="navItem">Contact Us</li>
+          {/* <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li> */}
+        </ul>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/champions" element={<Champions />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,16 +5,7 @@ import { SvgIcon } from "@mui/material";
 const Nav = () => {
   return (
     <>
-      <nav className="navContainer">
-        <h2 className="title">Summoner Search</h2>
-        <ul className="navList">
-          <li className="navItem">Home</li>
-          <li className="navItem">Champions</li>
-          <li className="navItem">Match History</li>
-          <li className="navItem">Contact Us</li>
-          {/* <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li> */}
-        </ul>
-      </nav>
+
     </>
   );
 };
