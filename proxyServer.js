ff5c0804b3c59766a9e9d568dfbdefc8c30541cc
Connor <endsleyconnor95@gmail.com>
2024-05-10 16:33:00 -0500

    proxy server working

--- proxyServer.js
+++ proxyServer.js
@@ -1,37 +1,27 @@
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
-const API_KEY = "RGAPI-79d88b4a-b61a-4669-849e-7645cd486c6c"
+const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 4000;
 
+const riotApiKey = "RGAPI-79d88b4a-b61a-4669-849e-7645cd486c6c";
+
+app.use(express.json());
 app.use(cors());
 
-const port = 4000;
+app.get('/api/summoner', async (req, res) => {
+  try {
+    const { gameName, tagLine } = req.query;
+    const url = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${riotApiKey}`;
+    const response = await axios.get(url);
+    console.log(response)
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
-app.listen(port, function(){
-    console.log("Listening on localhost 4000")
-})
-
-// function to get a players ID based on their username input
-
-// const getPlayerPUUID = (playerName) => {
-//    let puuidURL = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${playerName}?api_key=${API_KEY}`
-//     return axios.get(puuidURL)
-//     .then(response => {
-//         console.log(response.data);
-//         return
-//     }).catch(err=>err)
-// }
-
-let puuidURL = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/a%20schmelly%20goat?api_key=${API_KEY}`
-axios.get(puuidURL)
-.then(response => {
-    let puuId = response.data.puuid
-    console.log("PUUID:", puuId);
-    return puuid
-}).catch(err=>err)
-
-
-
-// getPlayerPUUID("a schmelly goat");
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import Home from './components/Home';
 import Nav from './components/Nav';
+import Champions from './components/Champions'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
